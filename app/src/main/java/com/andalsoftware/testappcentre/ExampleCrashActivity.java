94221d00cf0369fe960359e516d33b02f6d94bbe
stevensiahaan <steven@andalsoftware.com>
2018-10-31 08:48:11 +0700

    add simple test

--- app/src/main/java/com/andalsoftware/testappcentre/ExampleCrashActivity.java
+++ app/src/main/java/com/andalsoftware/testappcentre/ExampleCrashActivity.java
@@ -5,12 +5,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.microsoft.appcenter.crashes.Crashes;
 
 public class ExampleCrashActivity extends BaseActivity {
 
-    private Button btnCrash;
+    private Button btnCrash, btnOK;
     private TextView txtTextHello;
 
     @Override
@@ -18,7 +19,13 @@ public class ExampleCrashActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_example_crash);
         btnCrash = (Button) findViewById(R.id.btnCrash);
-
+        btnOK = (Button)findViewById(R.id.btnOK);
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ExampleCrashActivity.this, "OK", Toast.LENGTH_SHORT).show();
+            }
+        });
         btnCrash.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
